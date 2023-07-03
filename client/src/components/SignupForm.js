bcca3d3de406dadd47fb89dc2e6193c886d2de91
El Jefe <Tagne13@gmail.com>
2023-07-03 13:49:40 -0400

    replaced the loginUser() functionality imported from the API file with the LOGIN_USER mutation functionality

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-// import { createUser } from '../utils/API';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
@@ -41,7 +39,7 @@ const SignupForm = () => {
 
     try {
       const { data } = await addUser({
-        variables: { ...userFormData },
+        variables: { ...userFormData }
       });
 
       Auth.login(data.addUser.token);
